73511bbb4dc995837e6d2b639f3a0262820f6cd6
LinaAoyama <44237633+LinaAoyama@users.noreply.github.com>
2025-01-17 16:29:47 -0700

    CPER spider diagram by long-term treatment effects

--- Data_analysis/LTAR_indicator_spider_diagrams.R
+++ Data_analysis/LTAR_indicator_spider_diagrams.R
@@ -1,10 +1,10 @@
 #spider diagrams
 library(fmsb)
+library(tidyverse)
 
 #CPER 
 #calculate max and min
 CPER_maxmin <- data.frame(
-  Year = c("Max", "Min"),
   Treatment = c("Max", "Min"),
   Beef_Production = c(28.3, 9.9),
   C4 = c(1191.5467, 166.1279),
@@ -13,9 +13,41 @@ CPER_maxmin <- data.frame(
   Grassland_Sparrow = c(3.3260870, 0.3958333)
 )
 
-#combine data and max/min table
-CPER_all <- rbind(CPER_maxmin, CPER_combined_simple)
 
-radarchart(CPER_all[1:4,3:7],
+#pre- and post-treatment differentiate by variable
+#beef production's pre-treatment data is 2013 and treatment effect is 2014-2023
+#veg and bird responses' pre-treatment data is 2013 and 2014 and treatment effect is 2015-2023
+CPER_beef_ave <- CPER_beef_long %>%
+  mutate(Pre_Post = case_when(Year == 2013 ~ "Pre", 
+                              Year > 2013 ~ "Post")) %>%
+  group_by(Treatment, Pre_Post) %>%
+  summarize(Beef_Production = mean(Beef_Production))
+CPER_veg_bird_ave <- CPER_combined_simple %>%
+  select(Year, Treatment, C4, C3, Forage, Grassland_Sparrow) %>%
+  mutate(Pre_Post = case_when(Year < 2015 ~ "Pre", 
+                              Year > 2014 ~ "Post")) %>%
+  group_by(Treatment, Pre_Post) %>%
+  summarize(C4 = mean(C4),
+            C3 = mean(C3),
+            Forage = mean(Forage),
+            Grassland_Sparrow = mean(Grassland_Sparrow))
+CPER_average <- CPER_beef_ave %>%
+  left_join(CPER_veg_bird_ave, by = c("Treatment", "Pre_Post"))
+CPER_pre_summary <- CPER_average %>%
+  group_by(Pre_Post)%>%
+  summarise(Beef_Production = mean(Beef_Production),C4 = mean(C4),C3 = mean(C3),
+            Forage = mean(Forage),Grassland_Sparrow = mean(Grassland_Sparrow)) %>%
+  filter(Pre_Post == "Pre") %>%
+  mutate(Treatment = "Pre")
+
+#combine data and max/min table
+CPER_all <- rbind(CPER_maxmin,CPER_pre_summary[,-1], CPER_average[-c(2,4), -2])
+  
+#create spider diagrams
+radarchart(CPER_all[,2:6],
            cglty = 1, cglcol = "gray",
-           pcol = c(4,3), plwd = 2)
+           pcol = c(4,3, 2), plwd = 2)
+
+radarchart(CPER_all[-3,2:6], #remove pre-treatment
+           cglty = 1, cglcol = "gray",
+           pcol = c(3, 2), plwd = 2) #Aspirational practice reduced beef production but increased C3 grass. No change in grassland sparrow counts or forage.
